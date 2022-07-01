be1530c4c6e21b30f31f0576ab6fd1f18ce3017b
Hussam Eldin Wael <hussam.wael@mecaegypt.com>
2022-07-01 23:04:38 +0200

    .

---
+++ Server2/src/ImageEncoder.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Base64;
+
+public class ImageEncoder {
+
+
+    public static ArrayList<String> encodeImage(String ImagePath) throws Exception{
+        String arr [] = ImagePath.split("\\\\");
+        ArrayList<String> r = new ArrayList<String>() ;
+        FileInputStream imageStream = new FileInputStream(ImagePath);
+        byte[] myImageInBytes= imageStream.readAllBytes();
+        String myImageInString = Base64.getEncoder().encodeToString(myImageInBytes);
+
+        r.add(myImageInString); r.add(arr[arr.length-2] + "\\"+arr[arr.length-1]);
+        imageStream.close();
+        return r;
+    }
+
+    public static void decodeImage(ArrayList<String> r,String savingPath) throws Exception{
+
+
+        byte[] myImage =  Base64.getDecoder().decode(r.get(0));
+        FileOutputStream fileOutputStream = new FileOutputStream(savingPath+"\\"+r.get(1));
+        fileOutputStream.write(myImage);
+        fileOutputStream.close();
+
+    }
+
+
+
+
+}
---
+++ Server2/src/handler.java
@@ -0,0 +1,193 @@
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class handler implements Runnable{
+
+    private static final int userNameNotExists = 1;
+    private static final int incorrectPassword = 2;
+    private static final int validLogin = 3;
+
+    private Communicator communicator;
+
+    public handler(Socket clientSocket) throws IOException
+    {
+
+        this.communicator = new Communicator(clientSocket);
+    }
+
+    @Override
+    public void run() {
+
+
+        while(true)
+        {
+
+            String request = communicator.receiveMSG();
+            //No Requests Arrived So Wait
+            if(request == null)
+            {
+                continue;
+            }
+
+
+            else if (request.contains("Quit"))
+            {
+                System.out.println("Terminating This Thread");
+                //communicator.sendMSG("Quiting");
+                Server_Market.terminateT(this);
+                break;
+            }
+
+            else
+            {
+                handleClient(request);
+            }
+
+
+        }
+
+        communicator.terminateCommunication();
+
+    }
+
+
+    private void handleClient(String request)
+    {
+        ServiceProvider provider = new ServiceProvider(communicator);
+
+        if(request.contains("Check Login"))
+        {
+            System.out.println("Logging In");
+            provider.login();
+        }
+
+        else if (request.contains("Add New Account"))
+        {
+            System.out.println("Add new account");
+            provider.CreateNewAccount();
+        }
+
+
+        else if (request.contains("Get Client Information"))
+        {
+            System.out.println("Getting Client Information");
+            provider.ViewAccountInformation();
+        }
+
+
+        else if (request.contains("Buy Product"))
+        {
+            //Enter Critical Section
+            System.out.println("Buying Product");
+            communicator.sendMSG("Buying Product");
+
+            //Exit Critical Section
+        }
+
+        else if(request.contains("Search for Items")){
+            System.out.println("Searching for Items");
+            provider.SearchForItem();
+
+        }
+
+        else if(request.contains("Purchase")){
+            System.out.println("Purchasing Items");
+            provider.purchaseItems();
+
+        }
+
+        //"Deposit Cash"
+
+        else if (request.contains("Deposit Cash"))
+        {
+            System.out.println("Depositing Cash");
+            provider.DepositCash();
+        }
+
+        //Get Category Report
+        //Get Orders Report
+        else if (request.contains("Get Category Report"))
+        {
+            System.out.println("Sending Category Report");
+            provider.sendCategoryReport();
+        }
+
+
+
+        else if (request.contains("Get Orders Report"))
+        {
+            System.out.println("Sending Orders Report");
+            provider.sendOrderReport();
+        }
+
+        else if (request.contains("View User History"))
+        {
+            System.out.println("Sending History");
+            provider.sendHistory();
+        }
+
+
+        else if (request.contains("Quit"))
+        {
+
+        }
+
+        else
+        {
+            System.out.println("Invalid Request");
+            System.out.println(request);
+            communicator.sendMSG("Invalid");
+        }
+
+
+    }
+
+    private Account getUserNameAndPassword()
+    {
+        System.out.println("Waiting For U and P");
+
+        String UN = communicator.receiveMSG();
+        String Pass = communicator.receiveMSG();
+
+        Account AccountToValidate = new Account(UN,Pass);
+        return  AccountToValidate;
+    }
+
+
+
+    //Functions to Validate The User Account
+
+    private void ValidateUserAccount()
+    {
+        int x = Server_Market.findClientID(this);
+        System.out.println("Checking Login for "+String.valueOf(x));
+        Account newAccountToValidate = getUserNameAndPassword();
+        //For Testing
+
+        communicator.sendMSG("You Sent User Name: "+newAccountToValidate.getUserName());
+        communicator.sendMSG("You Sent Password : "+newAccountToValidate.getPassword());
+        //End Of For Testing
+
+        //Call Function from Server Market
+        //Get Result
+        //Send Result
+
+        //In Real Case Valid Or Invalid
+        communicator.sendMSG("Finished Checking Login");
+        System.out.println("Finished");
+    }
+
+    //Functions to Create New User Account
+    private void CreateNewUserAccount()
+    {
+        int x = Server_Market.findClientID(this);
+        System.out.println("Adding New Account for "+String.valueOf(x));
+        communicator.sendMSG("Adding New Account");
+
+    }
+
+
+
+
+}
